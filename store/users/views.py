e827c7666cfcef5501b31e11ac9d8c71cd0201c2
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-11-28 09:53:17 +0300

    Added loginrequaried decorator and filters fo categories

--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -43,6 +45,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(
