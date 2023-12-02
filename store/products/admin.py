78d376eb4a962748a7690213ba5c676b44f70280
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-12-02 22:05:11 +0300

    Edited admin panel for Basket and User

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
-from products.models import Product, ProductCategory
+from products.models import Basket, Product, ProductCategory
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name',
-        'description',
         'price',
         'quantity',
         'category',
     )
+    fields = (
+        'name',
+        'description',
+        ('price',
+         'quantity'),
+        'category',
+        )
     search_fields = (
         'name',
     )
@@ -19,6 +25,7 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'category',
     )
+    ordering = ('-name',)
 
 
 @admin.register(ProductCategory)
@@ -29,4 +36,12 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     )
     list_filter = (
         'name',
+    )
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = (
+        'products',
+        'quantity',
     )
--- store/users/admin.py
+++ store/users/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from products.admin import BasketAdmin
 from users.models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdnin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'username',
+    )
+    inlines = (
+        BasketAdmin,
+    )
