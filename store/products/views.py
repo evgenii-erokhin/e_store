e822d4594edce2f46a1c67ec54f58e9e5b19730a
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-12-05 21:40:06 +0300

    Created draft of IndexView based on class

--- store/products/views.py
+++ store/products/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
 
 from products.models import Product, ProductCategory, Basket
 from store.settings import PER_PAGE
+
+
+class IndexView(TemplateView):
+    pass
 
 
 def index(request):
