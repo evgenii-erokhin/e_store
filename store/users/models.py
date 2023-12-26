d2bf13b888efc467e450432115f8f992b5db1d34
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-12-26 20:36:39 +0300

    Added new model for email verification

--- store/users/models.py
+++ store/users/models.py
@@ -5,7 +5,38 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     users_images = models.ImageField(
+        'Изображение пользователя',
         upload_to='users_image/',
         blank=True,
         null=True
     )
+    is_verified_email = models.BooleanField(
+        'Подтвердил ли пользователь свой email',
+        default=False
+    )
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(
+        'Код подтверждения email',
+        unique=True
+    )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    created = models.DateTimeField(
+        'Время создания кода верификации',
+        auto_now_add=True
+    )
+    expiration = models.DateTimeField(
+        'Время истечения срока действия кода верификации'
+    )
+
+    class Meta:
+        verbose_name = 'Код верификации имейла'
+        verbose_name_plural = 'Код верификации имейла'
+
+    def __str__(self):
+        return f'Email verification object for {self.user.username}'
